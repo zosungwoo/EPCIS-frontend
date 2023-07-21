2a9f747229b2f87eace98bf8e0728c3b06cec695
zosungwoo <seouk77@gmail.com>
2023-07-21 11:02:30 +0900

    fix index, xmlCapture page

--- js/server-conn.js
+++ js/server-conn.js
@@ -10,13 +10,56 @@ $(document).ready(() => {
     crossOrigin: true,
   })
     .done((result) => {
-      baseURLs.xmlCaptureBaseURL = result.xmlCaptureBase;
-      baseURLs.soapQueryBaseURL = result.soapQueryBase;
-      baseURLs.soapSubscribeBaseURL = result.soapSubscribeBase;
-      baseURLs.jsonCaptureBaseURL = result.jsonCaptureBase;
-      baseURLs.restQueryBaseURL = result.restQueryBase;
-      baseURLs.restSubscribeBaseURL = result.restSubscribeBase;
-      
+      // [URL, (capture or query)]
+      baseURLs.xmlCaptureBaseURL = [result.xmlCaptureBase, "capture"];
+      baseURLs.soapQueryBaseURL = [result.soapQueryBase, "query"];
+      baseURLs.soapSubscribeBaseURL = [result.soapSubscribeBase, "query"];
+      baseURLs.jsonCaptureBaseURL = [result.jsonCaptureBase, "capture"];
+      baseURLs.restQueryBaseURL = [result.restQueryBase, "query"];
+      baseURLs.restSubscribeBaseURL = [result.restSubscribeBase, "query"];
+      $("#eventsCount").html(result.eventCount);
+      $("#vocabularyCount").html(result.vocabularyCount);
+
+      for (const URL in baseURLs) {
+        const regex = /(\w+)BaseURL/;
+        let divId = URL.match(regex)[1];
+
+        $.ajax({
+          url: baseURLs[URL][0],
+          crossOrigin: true,
+        })
+          .done(() => {
+            $("#" + divId + "Resp").addClass("bg-success");
+          })
+          .fail(() => {
+            $("#" + divId + "Resp").addClass("bg-danger");
+          })
+          .always(() => {
+            $("#" + divId + "Endpoint").html(
+              baseURLs[URL][0] + "/" + baseURLs[URL][1]
+            );
+          });
+      }
     })
-    .fail();
+    .fail(() => {
+      $(".rounded-circle").addClass("bg-danger");
+    });
 });
+
+function resetDB() {
+  let responseToast = $("#responseToast");
+
+  $.ajax({
+    url: baseURL + "/reset",
+    crossOrigin: true,
+  })
+    .done(function (result) {
+      responseToast.find(".toast-body").html("200 OK");
+    })
+    .fail(function (result) {
+      responseToast.find(".toast-body").html(result.responseText);
+    })
+    .always(function () {
+      responseToast.toast("show");
+    });
+}
